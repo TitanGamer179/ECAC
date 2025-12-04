133152fe4954b88a13da1ee4bf09fc6818e01302
TitanGamer179 <167643272+TitanGamer179@users.noreply.github.com>
2025-12-04 01:05:44 +0000

    update

--- Trabalho/mainActivityB.py
+++ Trabalho/mainActivityB.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import openfile
 import graficoB
+import calculo
 
 def main():
     print("\n--Requisito 1.1: Análise e Visualizção do Balanço de Classes ---\n")
@@ -14,10 +15,11 @@ def main():
         print("Nenhum dado foi carregado. Verifique o caminho da diretoria.")
         return
     print("Ficheiros abertos com sucesso!")
-    all_activity = all_data[:, 11]
-    activity_labels = all_activity[all_activity <=7]
-    graficoB.generate_examples(activity_labels)
-    print("Gráfico de balanço de classes gerado com sucesso!")
+    all_data=calculo.add_magnitude(all_data);
+    all_data=all_data[all_data[:, -1] <= 7] 
+    print("Dados filtrados com sucesso!")
+    feats,lables,parts=calculo.extrair_features(all_data)
+    segs_raw, labels_seg, parts_seg = calculo.segmentation(all_data)
 
 if __name__ == "__main__":
     main()
